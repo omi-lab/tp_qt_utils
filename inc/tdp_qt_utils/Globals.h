e6d40cdb8d00b1ffe89871251f025ebe96ec80d4
Tom Paynter <tompaynter@tdpe.co.uk>
2018-09-18 11:24:50 +0100

    Merged to public repos.

--- inc/tdp_qt_utils/Globals.h
+++ inc/tdp_qt_utils/Globals.h
@@ -4,56 +4,17 @@
 #include "tp_utils/StringID.h"
 
 #include <QStringList>
+#include <QDebug>
 
 #include <QtCore/qglobal.h>
 
-#ifndef TDP_QT_UTILS_SHARED_EXPORT
+#include <ostream>
+
 #if defined(TDP_QT_UTILS_LIBRARY)
-#  define TDP_QT_UTILS_SHARED_EXPORT Q_DECL_EXPORT
+#  define TDP_QT_UTILS_SHARED_EXPORT
 #else
-#  define TDP_QT_UTILS_SHARED_EXPORT Q_DECL_IMPORT
+#  define TDP_QT_UTILS_SHARED_EXPORT
 #endif
-#endif
-
-#ifndef TDP_CPP_VERSION
-#define TDP_CPP_VERSION 11
-#endif
-
-//##################################################################################################
-//TDP_NODISCARD
-#define TDP_NODISCARD __attribute__((warn_unused_result))
-
-//##################################################################################################
-//TDP_DEPRECATED
-#if TDP_CPP_VERSION>=14
-#define TDP_DEPRECATED(typ, func) [[deprecated]] typ func
-#else
-#ifdef __GNUC__
-#define TDP_DEPRECATED(typ, func) typ func __attribute__ ((deprecated))
-#elif defined(_MSC_VER)
-#define TDP_DEPRECATED(typ, func) typ __declspec(deprecated) func
-#else
-#define TDP_DEPRECATED(typ, func) typ func
-#endif
-#endif
-
-//##################################################################################################
-//TDP_FALLTHROUGH
-#if TDP_CPP_VERSION>=17
-#  define TDP_FALLTHROUGH [[fallthrough]]
-#elif __GNUC__>=7
-#  define TDP_FALLTHROUGH [[gnu::fallthrough]]
-#elif defined __CLANG__
-#  define TDP_FALLTHROUGH [[clang::fallthrough]]
-#else
-#  define TDP_FALLTHROUGH
-#endif
-
-//##################################################################################################
-//TDP_SIZEOF
-template<int s> struct __TDP_SIZEOF;
-//! Use this to show the size of objects at compile time, will fail at the line it is used.
-#define TDP_SIZEOF(o)__TDP_SIZEOF<sizeof(o)> __tdp_sizeof;
 
 //##################################################################################################
 //! Qt classes used throughout the rest of Tdp Toolkit.
@@ -68,5 +29,31 @@ TDP_DECLARE_ID(     dictionarySID,       "Dictionary")
 QStringList convertStringList(const std::vector<std::string>& src);
 
 }
+
+//##################################################################################################
+namespace detail
+{
+template<typename T>
+std::ostream& print(std::ostream& os, const T& v)
+{
+  QString str;
+  {
+    QDebug{&str} << v;
+  }
+
+  os << str.toStdString();
+
+  return os;
+}
+}
+
+template <typename K, typename V>
+inline std::ostream& operator<<(std::ostream& os, const QMap<K,V>& v){return detail::print(os, v);};
+template <typename V>
+inline std::ostream& operator<<(std::ostream& os, const QVector<V>& v){return detail::print(os, v);};
+template <typename V>
+inline std::ostream& operator<<(std::ostream& os, const QList<V>& v){return detail::print(os, v);};
+inline std::ostream& operator<<(std::ostream& os, const QString& v){return detail::print(os, v);};
+inline std::ostream& operator<<(std::ostream& os, const QByteArray& v){return detail::print(os, v);};
 
 #endif
